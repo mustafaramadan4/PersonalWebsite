857edcd3bbbe606b0e2dd03800157beb1c85df16
Mustafa Ramadan <ramadan.m@husky.neu.edu>
2020-06-11 15:20:42 -0700

    completed the image grid on index.html of 1,2 and 4 images per row

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -4,28 +4,19 @@ var i;
 
 function one() {
   for (i=0; i< elements.length; i++) {
-    elements[i].style.flex = "100%";
+    elements[i].style.flex = "90%";
   }
 }
 
 function two() {
   for (i=0; i< elements.length; i++) {
-    elements[i].style.flex = "50%";
+    elements[i].style.flex = "40%";
   }
 }
 
 function four() {
   for (i=0; i< elements.length; i++) {
-    elements[i].style.flex = "25%";
+    elements[i].style.flex = "20%";
   }
 }
 
-var gridhead = document.getElementById("gridheader");
-var btns = gridhead.getElementsByClassName("btn");
-for (var i= 0; i< btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
